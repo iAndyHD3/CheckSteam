ab49a738da0a72567a3892d2a59edc62630ca952
iAndyHD3 <marcaraus@gmail.com>
2023-11-10 21:02:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CheckSteam
+
+check steam
---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/ui/BasedButtonSprite.hpp>
+#include <Geode/utils/casts.hpp>
+
+using namespace geode::prelude;
+
+class $modify(MenuLayer)
+{
+	bool init()
+	{
+		if(!MenuLayer::init()) return false;
+		
+		auto menu = this->getChildByID("bottom-menu");
+		if(!menu) return true;
+
+		auto ngBtn = geode::cast::safe_cast<CCMenuItemSpriteExtra*>(menu->getChildByID("newgrounds-button"));
+		if (!ngBtn) return true;
+
+
+		auto spr = CircleButtonSprite::createWithSprite(
+			"steam.png"_spr,
+			1.0f,
+			geode::CircleBaseColor::Green,
+			geode::CircleBaseSize::MediumAlt
+		);
+		
+		ngBtn->setNormalImage(spr);
+
+		return true;
+	}
+
+	void onNewgrounds(CCObject*)
+	{
+		geode::createQuickPopup(
+			"Check Steam",            // title
+			"Are you sure you want to\n<cr>Check Steam</c>?",   // content
+			"Cancel", "Yes",      // buttons
+			[](auto, bool btn2)
+			{
+				if (btn2)
+				{
+					cocos2d::CCApplication::sharedApplication()->openURL("steam://store/322170");
+				}
+			}
+		);
+	}
+};
